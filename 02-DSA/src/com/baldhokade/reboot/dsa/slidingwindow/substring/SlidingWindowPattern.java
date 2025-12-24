995c766c14ee2f5197adc03ec00e8551b596c315
Balasaheb Dhokade <balasaheb.dhokade@gmail.com>
2025-12-24 12:58:34 +0530

    Day 07 Data structure

--- 02-DSA/src/com/baldhokade/reboot/dsa/slidingwindow/substring/SlidingWindowPattern.java
+++ 02-DSA/src/com/baldhokade/reboot/dsa/slidingwindow/substring/SlidingWindowPattern.java
@@ -1,4 +1,4 @@
-package com.baldhokade.reboot.dsa.substring;
+package com.baldhokade.reboot.dsa.slidingwindow.substring;
 
 import java.util.*;
 
@@ -174,4 +174,94 @@ public class SlidingWindowPattern {
 
     return maxLength;
   }
+
+  public static int longestSubstringExactlyKDistinct(String s, int k) {
+    if (s == null || s.isEmpty() || k <= 0){
+      return 0;
+    }
+
+    // consider freq map only contains distinct character with one or more occurrences
+    Map<Character, Integer> freq = new HashMap<>();
+    int left = 0, best = 0;
+
+    for (int right = 0; right < s.length(); right++) {
+      char c = s.charAt(right);
+      // add c
+      freq.put(c, freq.getOrDefault(c, 0) + 1);
+
+      // shrink while distinct > k
+      while (freq.size() > k){
+        char leftChar = s.charAt(left);
+        freq.put(leftChar, freq.get(leftChar) - 1);
+        if (freq.get(leftChar) == 0) {
+          freq.remove(leftChar);
+        }
+        left++;
+      }
+      // if distinct == k, update best
+      if (freq.size() == k){
+        best = Math.max(best, right - left + 1);
+      }
+    }
+
+    return best;
+  }
+
+  /**
+   * minimum window problem
+   * @param s - main string
+   * @param t - required chars string in substring of s
+   * @return smallest substring of s that contains all char of t
+   */
+  public static String minWindow(String s, String t){
+    if(s == null || s.isEmpty() || t == null || t.isEmpty()){
+      return "";
+    }
+
+    Map<Character, Integer> requiredCharCount = new HashMap<>();
+    char tc;
+    for (int i = 0; i < t.length(); i++){
+      tc = t.charAt(i);
+      requiredCharCount.put(tc, requiredCharCount.getOrDefault(tc, 0) + 1);
+    }
+
+    int totalRequiredChar = requiredCharCount.size();
+
+    Map<Character, Integer> currentCharCount = new HashMap<>();
+
+    int left = 0, bestStart = 0, bestLen = Integer.MAX_VALUE, requiredCharSatisfied = 0;
+
+    for(int right = 0; right < s.length(); right++){
+      char rsc = s.charAt(right);
+
+      currentCharCount.put(rsc, currentCharCount.getOrDefault(rsc, 0) + 1);
+
+      if(requiredCharCount.containsKey(rsc) && currentCharCount.get(rsc).intValue() == requiredCharCount.get(rsc).intValue()){
+        requiredCharSatisfied++;
+      }
+
+      while(left <= right && totalRequiredChar == requiredCharSatisfied){
+        int windowLen = right - left + 1;
+        if(windowLen < bestLen){
+          bestLen = windowLen;
+          bestStart = left;
+        }
+
+        char lsc = s.charAt(left);
+        currentCharCount.put(lsc, currentCharCount.get(lsc) - 1);
+        if (currentCharCount.get(lsc) == 0){
+          currentCharCount.remove(lsc);
+        }
+
+        if(requiredCharCount.containsKey(lsc) && currentCharCount.getOrDefault(lsc, 0) < requiredCharCount.get(lsc)){
+          requiredCharSatisfied--;
+        }
+
+        left++;
+      }
+
+    }
+    return bestLen == Integer.MAX_VALUE ? "" : s.substring(bestStart, bestStart + bestLen);
+  }
+
 }
